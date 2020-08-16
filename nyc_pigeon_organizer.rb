7cbffe167a30aec2b4d368f91219512c57e0874e
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-16 03:50:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ result_array= data.each_with_object({}) do |(key, value), result_array|
          if !result_array[name][key]
            result_array[name][key]=[]
          end
-         result_array[name][key].push(inner_key)
+         result_array[name][key].push(inner_key.to_s)
   
   end
 end
